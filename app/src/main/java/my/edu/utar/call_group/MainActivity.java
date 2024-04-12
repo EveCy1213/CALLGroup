c31971b9f474edf54ddf5f8edcf8c6be508f0836
jiaji06 <leejiaji06@1utar.my>
2024-04-13 02:43:08 +0800

    remove redundant imports

--- app/src/main/java/my/edu/utar/call_group/MainActivity.java
+++ app/src/main/java/my/edu/utar/call_group/MainActivity.java
@@ -1,28 +1,10 @@
 package my.edu.utar.call_group;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioGroup;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final int SPLASH_DISPLAY_LENGTH = 2000;
