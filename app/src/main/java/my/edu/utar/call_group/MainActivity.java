673c6b6c4e3a580a99b8b4922f1d66c5bdcd228f
jiaji06 <leejiaji06@1utar.my>
2024-04-13 02:37:30 +0800

    Revert "activity_main.xml, activity_login.xml, activity_register.xml, activity_forgot_password.xml"
    This reverts commit 09e69af7750467de812af2761de605266519334e.

--- app/src/main/java/my/edu/utar/call_group/MainActivity.java
+++ app/src/main/java/my/edu/utar/call_group/MainActivity.java
@@ -9,7 +9,6 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,25 +26,11 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final int SPLASH_DISPLAY_LENGTH = 2000;
-    private static final int IMAGE_DELAY = 500;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-            // Find the ImageView in your layout
-            ImageView imageView = findViewById(R.id.imageViewLogo);
-
-            // Delayed task to show the image after the specified delay
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    // Set the visibility of the ImageView to VISIBLE
-                    imageView.setVisibility(View.VISIBLE);
-                }
-            }, IMAGE_DELAY);
 
         new Handler().postDelayed(new Runnable(){
             @Override
